dffe11ecc399ecc0645b0271e9721d28e032dc7a
oyinbolaj <oyinbobola18@gmail.com>
2024-01-07 09:26:28 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various computer types
+ * main - A program that prints the size of various comput
+er types
  * Return 0 (Success)
  */
 int main(void)
